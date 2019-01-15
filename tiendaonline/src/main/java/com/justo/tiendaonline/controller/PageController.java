2e5791d637fc9d6f09daa91184a7eefba7e72c5a
JerryCode <jerryjust16@hotmail.com>
2019-01-14 18:43:13 -0600

    Front Completed

--- tiendaonline/src/main/java/com/justo/tiendaonline/controller/PageController.java
+++ tiendaonline/src/main/java/com/justo/tiendaonline/controller/PageController.java
@@ -12,7 +12,24 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
+		return mv;
+	}
+
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
 		return mv;
 	}
 
@@ -25,14 +42,15 @@ public class PageController {
 //		mv.addObject("greeting", greeting);
 //		return mv;
 //	}
-	
-	@RequestMapping(value = "/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting") String greeting) {
-		if (greeting == null) {
-			greeting = "hello there";
-		}
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
-		return mv;
-	}
+
+//	@RequestMapping(value = "/test/{greeting}")
+//	public ModelAndView test(@PathVariable("greeting") String greeting) {
+//		if (greeting == null) {
+//			greeting = "hello there";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+
 }
